911885e98a246491d6aad8dcbdb6b5b161536896
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2019-11-17 23:02:34 +0800

    Create binary_search_tree_06170223.py

---
+++ HW3/binary_search_tree_06170223.py
@@ -0,0 +1,36 @@
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+        """
+        :type val: int
+        :type left: TreeNode or None
+        :type right: TreeNode or None
+        """
+class Solution(object):
+    def insert(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode(inserted node)
+        """
+    def delete(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: None Do not return anything, delete nodes(maybe more than more) instead.(cannot search())
+        """
+    def search(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode(searched node)
+        """
+    def modify(self, root, target, new_val):
+        """
+        :type root: TreeNode
+        :type target: int
+        :type new_val: int
+        :rtype: None Do not return anything, modify nodes(maybe more than more) in-place instead.(cannot search())
+        """
