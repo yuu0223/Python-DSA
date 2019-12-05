748af6300616945a33506cc726f1fce5e5a942af
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2019-12-05 12:38:42 +0800

    Update hash_table_06170223.py

--- HW4/hash_table_06170223.py
+++ HW4/hash_table_06170223.py
@@ -1,32 +1,99 @@
+from Cryptodome.Hash import MD5 as md5
+
 class ListNode:
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, value):
+        self.val = value
         self.next = None
-        """
-        :type val: int
-        :type next: ListNode
-        :rtype: None        
-        """
+            
 class MyHashSet:
+     
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = [None] * capacity
-        """
-        :type capacity: int
-        :rtype: None
-        """
+        print(self.data)
+        
+        
+    def md5(self, val): #將輸入的值加密
+        
+        new = md5.new()
+        new.update(val.encode('utf-8'))
+        finish = new.hexdigest()
+        return finish
+        
+    def ascii_sum(self, val): #將輸入的值轉為數字
+        
+        ascii_word = []
+        total_sum = 0
+        for i in val:
+            trans = ord(i)
+            ascii_word.append(trans)
+        print(ascii_word)
+       
+        b =len(ascii_word)-1
+        for a in ascii_word:
+            sum_word = a*(10**b)
+            total_sum = total_sum+sum_word
+            b=b-1
+        
+        print(total_sum)
+        return total_sum
+        
+    
+    def mod(self, val): #將輸入之值取mod，並放進哪個位置
+        
+        number = self.ascii_sum(val)
+
+        mod = number%self.capacity
+        print(mod)
+        
+        return mod
+        
     def add(self, key):
-        """
-        :type key: int
-        :rtype: None
-        """
+        
+        mod_key = self.mod(key)
+        md5_key = self.md5(key)
+        
+        if self.data[mod_key] == None:
+            dict = {}
+            self.data[mod_key] = dict
+            self.data[mod_key][key] = md5_key 
+            
+        else:
+            self.data[mod_key][key] = md5_key
+            
+        return self.data
+    
+    
     def remove(self, key):
-        """
-        :type key: int
-        :rtype: None
-        """
+        mod_key = self.mod(key)
+        md5_key = self.md5(key)
+        
+        if self.data[mod_key] == None:
+            return 
+            
+        else:
+            self.data[mod_key].pop(key)
+            
+        return self.data
+    
+    
     def contains(self, key):
-        """
-        :type key: int
-        :rtype: bool(True or False)
-        """
+        
+        mod_key = self.mod(key)
+        md5_key = self.md5(key)
+        
+        if self.data[mod_key] != None:
+            if key in self.data[mod_key]:
+                return True
+            
+            else:
+                return False
+            
+        else:
+            return False
+   
+
+#Reference:
+#https://www.itread01.com/content/1541137210.html
+#https://blog.techbridge.cc/2017/01/21/simple-hash-table-intro/
+#http://alrightchiu.github.io/SecondRound/hash-tableintrojian-jie.html
