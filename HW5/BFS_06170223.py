58bf76e34a3b7a4aef045954456d752f9c1b2fe4
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2019-12-10 12:03:40 +0800

    Create BFS_06170223.py

---
+++ HW5/BFS_06170223.py
@@ -0,0 +1,28 @@
+# Python3 Program to print BFS traversal 
+# from a given source vertex. BFS(int s) 
+# traverses vertices reachable from s. 
+from collections import defaultdict 
+  
+# This class represents a directed graph 
+# using adjacency list representation 
+class Graph:
+    # Constructor 
+    def __init__(self): 
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
+
+    # function to add an edge to graph 
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    # Function to print a BFS of graph 
+    def BFS(self, s): 
+        """
+        :type s: int
+        :rtype: list
+        """
+    def DFS(self, s):
+        """
+        :type s: int
+        :rtype: list
+        """
