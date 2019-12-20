3696c841eee873fdcbf0bb3823ebd90a83255e1e
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2019-12-20 11:31:41 +0800

    Update BFS_06170223.py

--- HW5/BFS_06170223.py
+++ HW5/BFS_06170223.py
@@ -1,28 +1,77 @@
-# Python3 Program to print BFS traversal 
-# from a given source vertex. BFS(int s) 
-# traverses vertices reachable from s. 
+ 
 from collections import defaultdict 
-  
-# This class represents a directed graph 
-# using adjacency list representation 
+
 class Graph:
-    # Constructor 
-    def __init__(self): 
-        # default dictionary to store graph 
+    
+    def __init__(self):
         self.graph = defaultdict(list) 
 
-    # function to add an edge to graph 
+        
     def addEdge(self,u,v): 
         self.graph[u].append(v) 
-  
-    # Function to print a BFS of graph 
-    def BFS(self, s): 
-        """
-        :type s: int
-        :rtype: list
-        """
+        return self.graph
+
+    
+    def BFS(self, s):
+        
+        store = [] #暫存的list
+        sure = [] #確定的list
+        
+        for a in range(len(self.graph)): #self.graph的長度=要排序的個數
+            if s not in store:
+                    if s not in sure:
+                        store.append(s) #若s皆不在暫存及確定的list中則新增進暫存的list
+        
+            for i in self.graph[s]: #[s]的位置上可能不只一個數,利用for迴圈一個一個讀出
+                if i not in store:
+                    if i not in sure:
+                        store.append(i) #一個一個新增進暫存的list
+        
+            sure.append(s)
+            store.pop(0) #將新增進確定的list從暫存區刪除
+           
+        
+            if len(sure) == len(self.graph): #sure的個數=self.graph個數代表已經排序好
+                return sure
+            
+            else:
+                s = store[0] 
+                
+                
     def DFS(self, s):
-        """
-        :type s: int
-        :rtype: list
-        """
+        
+        stack = []
+        sure = []
+        i = 1
+        
+        while len(sure) != len(self.graph):
+            
+            if s not in sure:
+                sure.append(s)
+                    
+            stack = self.graph[s]
+            
+            if stack == []: #若stack是空的就從sure中往回推一個元素
+                node = sure[-1-i]
+                i+=1
+                stack = self.graph[node]
+                
+            else:
+                node = stack[-1]
+            
+            if node not in sure:
+                sure.append(node)
+                stack.pop()
+                
+            else:
+                if stack !=[]: #若該點的stack不是空的
+                    if stack[-1] not in sure: #若stack最上層的元素不在sure中
+                        node = stack[-1]
+                    
+                    else:
+                        stack.pop() #若在就直接從stack中刪除掉
+            
+            
+            s = node #令下一個s為node
+            
+        return sure
