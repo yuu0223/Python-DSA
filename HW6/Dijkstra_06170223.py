7f2f41700b3985769b03c810cdba93b0b3dbe142
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2019-12-24 12:29:02 +0800

    Create Dijkstra_06170223.py

---
+++ HW6/Dijkstra_06170223.py
@@ -0,0 +1,23 @@
+from collections import defaultdict 
+ 
+class Graph(): 
+
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [] 
+        self.graph_matrix = [[0 for column in range(vertices)]  
+                    for row in range(vertices)] 
+    def addEdge(self,u,v,w): 
+        """
+        :type u,v,w: int
+        :rtype: None
+        """
+    def Dijkstra(self, s): 
+        """
+        :type s: int
+        :rtype: dict
+        """
+    def Kruskal(self):
+        """
+        :rtype: dict
+        """
