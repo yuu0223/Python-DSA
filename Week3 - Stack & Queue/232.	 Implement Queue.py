be89dd37549510194b7369298133dfe316b4d942
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2020-01-08 16:00:49 +0800

    Update 232.	 Implement Queue.py

--- Week3 - Stack & Queue/232.	 Implement Queue.py
+++ Week3 - Stack & Queue/232.	 Implement Queue.py
@@ -1,34 +1,27 @@
 class MyQueue:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        
         self.queue=[]
 
+        
     def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
+       
         self.queue.append(x)
         
 
     def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
+       
         return self.queue.pop(0) #pop(要移除的第n項)
 
+    
     def peek(self) -> int:
-        """
-        Get the front element.
-        """
+       
         return self.queue[0]
 
+    
     def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
+        
         return self.queue == []
 
 
@@ -38,3 +31,5 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+#reference:https://www.runoob.com/python/python-lists.html
