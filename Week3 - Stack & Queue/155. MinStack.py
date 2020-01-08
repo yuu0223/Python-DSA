3351850c9b5e5a32e1bbf1202f99db0e2e0d0f5e
yuu0223 <55571613+yuu0223@users.noreply.github.com>
2020-01-08 16:03:32 +0800

    Update 155. MinStack.py

--- Week3 - Stack & Queue/155. MinStack.py
+++ Week3 - Stack & Queue/155. MinStack.py
@@ -1,39 +1,37 @@
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.items=[]
-        self.items_min=[] #為了取得最小值所設
+        
+        self.stack=[]
+        self.minstack=[] #為了取得最小值所設
         
 
     def push(self, x: int) -> None:
         
-        self.items.append(x)
+        self.stack.append(x)
         
-        if len(self.items_min) == 0 or x <= self.items_min[-1]:
-            self.items_min.append(x) 
+        if len(self.minstack) == 0 or x <= self.minstack[-1]:
+            self.minstack.append(x) 
             #若最小值的list中無值可比較or輸入的x比item_min list中的數值小，則append
         
     def pop(self) -> None:
         
-        top = self.items[-1] #先設置top為哪一個list的
-        self.items.pop() 
+        top = self.stack[-1] #先設置top為哪一個list的
+        self.stack.pop() 
     
-        if top == self.items_min[-1]: 
-            self.items_min.pop()
+        if top == self.minstack[-1]: 
+            self.minstack.pop()
             #若items_min和items list中最後一項相同則須移除
        
 
     def top(self) -> int:
         
-        return self.items[-1] #列出最後一項
+        return self.stack[-1] #列出最後一項
         
 
     def getMin(self) -> int:
         
-        return self.items_min[-1] #列出items_min的最後一項取得最小值
+        return self.minstack[-1] #列出items_min的最後一項取得最小值
         
 
 
@@ -43,3 +41,5 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+#reference:https://www.runoob.com/python/python-lists.html
