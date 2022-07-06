aee92d7dfc6a7a12aaee3c4e9f1aa25cc5f29caa
Penny Chen <55571613+yuu0223@users.noreply.github.com>
2022-07-06 17:27:19 +0800

    Update 118_Pascal's_Triangle.py

--- Leetcode/118_Pascal's_Triangle.py
+++ Leetcode/118_Pascal's_Triangle.py
@@ -18,10 +18,12 @@ class Solution:
                 while len(total_list) < self.numRows:
                     total_list.append([])
                 
+                #先把所有位置填入1
                 for a in range(3,self.numRows+1,1):
                     for b in range(1,a+1,1):
                         total_list[a-1].append(1)
                 
+                #將巴斯卡三角形內需要替換的數字替換掉
                 for i in range(2,self.numRows,1):
                     for j in range(1,i,1):
                         add_num = total_list[i-1][j-1]+total_list[i-1][j]
